c7a30290c52bbd0d8115da1f56068cb5f01de1ab
Cameron Lindsay <caj.lindsay@gmail.com>
2023-03-07 10:28:47 +1300

    updated gke config

--- _config-gke/build-api.sh
+++ _config-gke/build-api.sh
@@ -2,3 +2,4 @@
 docker build -f _config-docker/Dockerfile.base -t starting-again-base:latest .
 docker build -f _config-docker/Dockerfile.api --build-arg app=$1 -t gcr.io/starting-again-377302/$1:latest .
 docker push gcr.io/starting-again-377302/$1:latest
+kubectl delete pods -l app=$1 --namespace starting-again
--- _config-gke/build-web.sh
+++ _config-gke/build-web.sh
@@ -2,3 +2,4 @@
 docker build -f _config-docker/Dockerfile.base -t starting-again-base:latest .
 docker build -f _config-docker/Dockerfile.web --build-arg app=$1 --build-arg mode=feature -t gcr.io/starting-again-377302/$1:latest .
 docker push gcr.io/starting-again-377302/$1:latest
+kubectl delete pods -l app=$1 --namespace starting-again
